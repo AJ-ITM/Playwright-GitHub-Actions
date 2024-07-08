17e5c154d21444d8634cfe23e6fd46f8cc967140
AJ-ITM <andrzej.jankowski@itmagination.com>
2024-07-08 09:17:53 +0200

    CI-fix-3

--- tests/data.ts
+++ tests/data.ts
@@ -1,16 +1,10 @@
 export const urls = [
-    'https://www.rp.pl/',
-    'https://www.onet.pl/',
     'https://www.nytimes.com/international/',
-    'https://www.nytimes.com/international/section/us',
     'https://www.bbc.com/news',
     'https://edition.cnn.com/'
 ];
 
 export const keywords = [
     'election',
-    'IT',
-    'wybory',
-    'biznes',
     'business',
 ];
